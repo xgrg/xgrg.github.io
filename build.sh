49511132c167cebfb9f5d582a0fe9361163b4942
xgrg <goperto@gmail.com>
2016-04-20 20:31:11 +0200

    test

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
-pelican content -o /tmp/output
-git config --local user.name xgrg
-git config --local user.email goperto@gmail.com
-git checkout master
-git reset --hard 4eda2f3e5b # back to the initial commit
-git push origin master --force
-mv LICENSE /tmp
-rm -rf *
-mv /tmp/LICENSE .
-cp -rf /tmp/output/* .
-git add --all
-git commit -m 'Update documentation'
-git push origin master
+#pelican content -o /tmp/output
+#git config --local user.name xgrg
+#git config --local user.email goperto@gmail.com
+#git checkout master
+#git reset --hard 4eda2f3e5b # back to the initial commit
+#git push origin master --force
+#mv LICENSE /tmp
+#rm -rf *
+#mv /tmp/LICENSE .
+#cp -rf /tmp/output/* .
+#git add --all
+#git commit -m 'Update documentation'
+#git push origin master
+pelican-themes -i themes/pure
+make github
